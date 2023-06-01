dd60101e449d8f122c7568c1dabd065d5bbeae47
trinhquan1004 <trinhquan04102002@gmail.com>
2023-06-02 02:06:36 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-curly-newline': 0,
+    'import/prefer-default-export': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@src', './src'],
+          ['@apis', './src/apis'],
+          ['@components', './src/components'],
+          ['@containers', './src/containers'],
+          ['@redux', './src/redux'],
+        ],
+        extensions: ['.js', '.jsx', '.json'],
+      },
+    },
+  },
+};
